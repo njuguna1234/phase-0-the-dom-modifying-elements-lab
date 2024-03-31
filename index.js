c09d02f72f59802fec1575f09eb4aa64fe17dc74
roy wahome <roy.njuguna@student.moringaschool.com>
2024-03-31 12:50:46 +0300

    the DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// index.js
+
+// Remove the DOM node with id 'main'
+document.querySelector('main#main').remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+const championName = "John"; 
+newHeader.textContent = `${championName} is the champion`;
+document.body.appendChild(newHeader);
